28da0e1c1f2efae02a88471a42813020f000526a
Juan <juanpmiranda92@gmail.com>
2020-02-23 17:15:02 -0300

    whole lotta work

--- js/index.js
+++ js/index.js
@@ -1,18 +1,32 @@
-var header = document.getElementById("header");
+window.onload = function() {
+  var sliders = document.getElementsByClassName('sliders');
+  console.log(sliders.length);
+};
 
-function showSlides() {
-    var slidesCollection = document.getElementsByClassName("slides");
-    console.log(slidesCollection.length);
+window.onscroll = function() {
+
+  var header = document.getElementById('header');
+  console.log(window.pageYOffset);
+  console.log(header.offsetTop);
+
+  if(window.pageYOffset >= 1600){
+    var basic = document.getElementById('basic');
+    basic.style.display = 'block';
+    basic.style.animation = 'showUpOnly 6s 1';
+
+    var pro = document.getElementById('pro');
+    pro.style.display = 'block';
+    pro.style.animation = 'showUpOnly 6s 1';
+  }
+
+  if (window.pageYOffset > header.offsetTop) {
+    header.classList.add("sticky")
+    document.querySelectorAll("menu-bar li a").classList.add('sticky-white-font');
+  } else {
+    header.classList.remove("sticky");
+  }
 }
 
+function slideShow(){
 
-window.onscroll = function(){
-    console.log(this.header.offsetTop);
-    if(window.pageYOffset >= this.header.offsetTop)
-    {
-        this.header.classList.add("sticky")
-    }
-    else{
-        this.header.classList.remove("sticky");
-    }
 }
